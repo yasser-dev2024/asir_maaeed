82a5b0dace1a45799df05456a89de393916d423e
ياسر <@example.com>
2026-06-21 15:47:09 +0300

    Deploy GitHub Pages

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'saif-seha-musaed-v1';
+const BASE_URL = self.registration.scope;
+const CORE_ASSETS = [
+  '',
+  'manifest.webmanifest',
+  'favicon.svg',
+  'maskable-icon.svg'
+].map((asset) => new URL(asset, BASE_URL).toString());
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          const copy = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return networkResponse;
+        })
+        .catch(() => caches.match(BASE_URL));
+    })
+  );
+});
